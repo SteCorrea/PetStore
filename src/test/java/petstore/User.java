ae647451187a1e30822c3c60ce792d8421dad7b0
stefa <stefany_scorrea@hotmail.com>
2021-08-13 19:55:44 -0300

    Inclusao do POST para incluirUsuario

---
+++ src/test/java/petstore/User.java
@@ -0,0 +1,34 @@
+package petstore;
+
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class User {
+
+    String uri = "https://petstore.swagger.io/v2/user";
+
+    public String LerJson(String caminhojson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhojson)));
+    }
+@Test
+    public void incluirUser() throws IOException {
+        String jsonBody = LerJson("db/user1.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+        ;
+    }
+}
